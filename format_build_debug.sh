133bf5b3038d2c8f1f16ee885b8d02da4d6ea5fe
Rahul Senna <rahulvsenna@gmail.com>
2026-06-03 08:38:43 +0530

    enabling metal validation in the debugger script

--- format_build_debug.sh
+++ format_build_debug.sh
@@ -1,2 +1,29 @@
+set -e
 clang-format -i src/*.h src/*.cpp src/*.mm src/*.metal
-./build.sh && lldb -b -o "run" -k "bt" ./build/engine
+
+./build.sh
+
+/usr/bin/log stream \
+    --level debug \
+    --predicate 'process == "engine" AND senderImagePath contains "Metal" AND NOT(eventMessage CONTAINS "DescriptorHeapNewHandle") AND NOT(eventMessage CONTAINS "MemoryUsed")' \
+    | tee build/metal.log &
+LOG_PID=$!
+
+echo "$LOG_PID"
+
+cleanup() {
+    kill -- -"$LOG_PID" 2>/dev/null || true
+}
+
+trap cleanup EXIT INT TERM
+
+
+# man MetalValidation
+
+# MTL_SHADER_VALIDATION_REPORT_TO_STDERR=1 \
+MTL_DEBUG_LAYER=1 \
+MTL_SHADER_VALIDATION=1 \
+METAL_DEVICE_WRAPPER_TYPE=1 \
+lldb -b -o "run" -k "bt" ./build/engine
+
+pgrep -x log | xargs kill
